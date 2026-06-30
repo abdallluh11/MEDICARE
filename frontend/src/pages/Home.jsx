eb73842f74836dade140b9f0a46b7eb42d938849
Abdalluh Elsawy <elsawyabdalluh@gmail.com>
2026-06-30 16:34:54 +0300

    feat: add Certification pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
+import Certification from "../components/Certification";
+
+
 const Home = () => {
   return (
     <div>
         <Navbar />
         <Banner />
+        <Certification />
     </div>
   );
 }
