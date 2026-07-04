ddee8d0452c445a60d58d69d30403b464ef53543
Abdalluh Elsawy <elsawyabdalluh@gmail.com>
2026-07-04 15:10:03 +0300

    feat: add ContactPage and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
 import DoctorDetail from "./pages/DoctorDetail";
 import Service from "./pages/Service";
+import ServiceDetailPage from "./pages/ServiceDetailPage";
+import  Contact  from "./pages/Contact";
+
 
 const App = () => {
   return (
@@ -14,6 +17,8 @@ const App = () => {
         <Route path="/doctors/:id" element={<DoctorDetail />} />
 
         <Route path="/services" element={<Service />} />
+        <Route path="/services/:id" element={<ServiceDetailPage />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
