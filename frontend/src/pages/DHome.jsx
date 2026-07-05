4209cfff5d08b8fd094f99273a9f1f7b4777c4b1
Abdalluh Elsawy <elsawyabdalluh@gmail.com>
2026-07-05 07:08:34 +0300

    feat: add DashboardPage and routing

--- frontend/src/pages/DHome.jsx
+++ frontend/src/pages/DHome.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import DoctorNavbar from '../doctor/DoctorNavbar'
+import DashboardPage from '../doctor/DashboardPage'
 
 const DHome = () => {
   return (
     <div>
         <DoctorNavbar />
+        <DashboardPage />
+        
     </div>
   )
 }
